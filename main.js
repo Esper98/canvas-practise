4a7064fdbd161d1021469ac874f291b3bdfdbaf3
esper van den heuvel <espervdheuvel@hotmail.com>
2018-11-09 22:44:36 +0100

    random spawning

--- main.js
+++ main.js
@@ -6,8 +6,54 @@ canvas.width = window.innerWidth;
 
 var c = canvas.getContext('2d');
 
-
+c.fillStyle = "rgba(255,0,0,0.4)";
 c.fillRect(10,10,50,50);
 c.fillRect(10,70,50,50);
 c.fillRect(70,10,50,50);
 c.fillRect(70,70,50,50);
+
+
+
+function randomColor(){
+    var color = ['red', 'blue', 'yellow', 'black', 'grey', 'green', 'orange']; 
+    var strokeStyle = color[Math.floor(Math.random() * color.length)]; 
+    return strokeStyle;
+}
+
+
+//random circles
+for (let i = 0; i < 100; i++) {
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+    c.beginPath();
+    c.arc(x,y,20,0,10,false);
+    c.strokeStyle = "blue";
+
+
+    c.strokeStyle = randomColor();
+
+    c.stroke();
+}
+
+
+
+function drawStrokes(){
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+    c.beginPath();
+    c.moveTo(x,y);
+
+    for (let i = 0; i < 10; i++) {
+        x = Math.random() * window.innerWidth;
+        y = Math.random() * window.innerHeight;
+    
+    
+        c.lineTo(x,y);
+    }
+    
+    c.strokeStyle = randomColor();
+    c.stroke();
+    //random strokes
+}
+
+drawStrokes();
