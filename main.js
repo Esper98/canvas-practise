0731036dd682dc018be430bdeb59deaace91da5f
esper van den heuvel <espervdheuvel@hotmail.com>
2018-11-27 22:45:25 +0100

    moving rotated text

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ canvas.width = window.innerWidth;
 var c = canvas.getContext('2d');
 var hideCircles = true;
 var circles = [];
+var words = [];
 var amountOfCircles = 500;
 amount.innerHTML = "Amount of cicrcles: " + amountOfCircles;
 
@@ -57,6 +58,10 @@ function init() {
         var dy = (Math.random() -0.5) * 4;
         circles.push(new Circle(x,y,dx,dy,radius));
     }
+    words = [];
+    words.push(new Word(100,100,10,10,"Canvas!"));
+    words.push(new Word(200,200,20,20,"2!"));
+
 }
 
 function randomColor(){
@@ -117,12 +122,53 @@ function Circle(x,y,dx,dy,radius){
     }
 }
 
+function Word(x,y,dx,dy,Char){
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = '#ffffff00';
+    this.hoverColor = randomColor();
+
+    this.draw = function(){
+        c.font = '30pt Arial';
+        c.save();
+        c.translate(this.x,this.y);
+        c.rotate(-0.5*Math.PI);
+        
+        var rText = Char;
+        c.fillText(rText , 0, 0);
+        c.restore();
+    }
+
+    this.update = function(){
+        if (this.x >= innerWidth || this.x <= 0){
+            this.dx = -this.dx;
+        }
+        if (this.y >= innerHeight || this.y <= 0){
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        if(hideCircles == false){
+            this.color = this.hoverColor;
+        }
+
+        this.draw();
+    }
+}
 
 function animate(){
     c.clearRect(0,0,innerWidth,innerHeight);
     circles.forEach(Circle => {
         Circle.update();
     });
+    words.forEach(Word => {
+        Word.update();
+        words.length;
+    });
+
     requestAnimationFrame(animate);
 }
 
